c780600857792ba4a1a9fc2c0471df3e8e58225b
Ian Volkov <y.volkov@g.nsu.ru>
2025-09-24 01:04:11 +0700

    removed smth

--- solution/input.sh
+++
@@ -1,8 +0,0 @@
-tmpdir=$(mktemp -d)
-echo "Вывести время в формате:
-15 час/а/ов 23 минут/ы" > $tmpdir/input.txt
-enconv $tmpdir/input.txt
-#iconv -t "koi8-r" -f "WINDOWS-1252" -o $tmpdir/output.txt $tmpdir/input.txt 
-#iconv -f CP866 -t UTF-8 $tmpdir/output.txt -o $tmpdir/output1.txt
-cat $tmpdir/input.txt
-rm -fr $tmpdir
